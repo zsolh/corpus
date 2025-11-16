ee87a406a9e8a0a1fa79e8cb7c15a1bdeca2127a
zsoltholman <holman.zsolt@gmail.com>
2025-11-17 00:26:20 +0100

    Initial commit

---
+++ scripts/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+from src.corpus import es_view
+
+app = es_view.app
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
